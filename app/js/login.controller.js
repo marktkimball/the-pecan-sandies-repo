113aea4a5d09dcc6446a8673cf056d73d83d1751
Sam Grayson <samuel.c.grayson17@gmail.com>
2015-08-29 02:39:48 -0400

    backend

--- app/js/login.controller.js
+++ app/js/login.controller.js
@@ -2,14 +2,21 @@
   'use strict';
   angular
     .module('beaut')
-    .controller('LoginController', function($scope, $route, LoginService){
+    .controller('LoginController', function($scope, $route, $location, LoginService){
       $scope.userInfo = {};
 
       $scope.login = function(event) {
         event.preventDefault;
         var userInfo = $scope.userInfo;
         console.log("Login user: ", userInfo);
-        LoginService.login(userInfo);
+        LoginService.login(userInfo).success(function(data){
+          if(data.email === userInfo.email) {
+            if (data.stylist === true) {
+              $location.path('/account');
+            } else {
+              $location.path('/feed');
+            }
+        });
       };
 
       $scope.signup = function(event){
